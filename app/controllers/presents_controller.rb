f11673346039cbe07622bea2fb150022c907b66b
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-23 18:12:19 +0100

    add list items

--- app/controllers/presents_controller.rb
+++ app/controllers/presents_controller.rb
@@ -3,4 +3,15 @@ class PresentsController < ApplicationController
     @available_presents = Present.all.select(&:available)
     @taken_presents = Present.all.reject(&:available)
   end
+
+  def reserve
+    @present = Present.find(params[:id])
+    if @present.available
+      @present.available = false
+    else
+      @present.available = true
+    end
+    @present.save
+    redirect_to root_path
+  end
 end
