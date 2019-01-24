052b90300a24734ba2c777094a0de6d682e9c702
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-24 11:30:49 +0100

    Stop modal from moving page and include present title

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,10 @@
 import "bootstrap";
+
+const reserve_btns = document.querySelectorAll('.modal-btn');
+reserve_btns.forEach( (btn) => {
+  btn.addEventListener('click', myfunction)
+})
+
+function myfunction(event) {
+  event.preventDefault();
+}
