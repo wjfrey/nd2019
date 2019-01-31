a93e32ef660b4fa941f3945245175836aae6d6ab
Wiebke Frey <frey.wiebke@googlemail.com>
2019-01-31 20:23:05 +0100

    fix pic bug

--- app/models/present.rb
+++ app/models/present.rb
@@ -1,3 +1,11 @@
 class Present < ApplicationRecord
   mount_uploader :photo, PhotoUploader
+  after_create :set_default_photo
+
+  def set_default_photo
+    if photo.file.nil?
+      self.remote_photo_url = "https://res.cloudinary.com/dqxb19mjv/image/upload/v1548960846/nd2019/present_placeholder.jpg"
+    end
+    self.save
+  end
 end
